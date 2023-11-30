529f076caf410092e653a45bcc507ddd21168e3d
kawashima <kawashima@cloudpack.jp>
2023-11-30 12:05:35 +0900

    chore: add version.go

--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import (
 	"os"
 )
 
-const (
-	Name     = "alpen"
-	Version  = "0.0.11"
-	Revision = "HEAD"
-)
-
 func main() {
 	if err := newApp().run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+const Name = "alpen"
+
+const Version = "0.0.11"
+
+var Revision = "HEAD"
