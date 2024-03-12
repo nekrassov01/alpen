d935ea5953639b1c33c0f0d6bc1e54a0b7b9b5d5
nekrassov01 <unclek62@gmail.com>
2024-03-12 10:27:45 +0900

    fix: add context

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 )
@@ -8,7 +9,7 @@ import (
 const Name = "alpen"
 
 func main() {
-	if err := newApp().cli.Run(os.Args); err != nil {
+	if err := newApp().cli.RunContext(context.Background(), os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("%s: %w", Name, err))
 		os.Exit(1)
 	}
